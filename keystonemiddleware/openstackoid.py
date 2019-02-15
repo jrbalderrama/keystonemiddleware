e9db0de6a53660e6905bc03a34b03255f5895be8
RFish ⚓ <RonanCherrueau@gmail.com>
2019-02-15 17:18:34 +0100

    [🌠oïd] Make it a bit simpler
    Let's rely on the Admin user instead of the conf user since
    we know every instances got the same admin user in this PoC.

--- keystonemiddleware/openstackoid.py
+++ keystonemiddleware/openstackoid.py
@@ -55,43 +55,16 @@ from keystoneauth1.session import Session
 from keystoneauth1.adapter import Adapter
 from keystonemiddleware.auth_token import _identity
 
-# Keystone clients. -- Is it shared between all services (i.e., nova, glance,
+# Keystone clients
 # ...) of one instance (e.g., InstanceOne) since we make it global?
 K_CLIENTS = {}
 
-def lift_conf(conf):
-    "Lifting of keystonemiddleware._common.config.Config into a dict."
-    def get_or_else(key, orelse=None):
-        try:
-            return conf.get(key)
-        except cfg.NoSuchOptError:
-            return orelse
-
-    return {
-        "domain_id": get_or_else("domain_id"),
-        "domain_name": get_or_else("domain_name"),
-        "password": get_or_else("password"),
-        "project_domain_id": get_or_else("project_domain_id"),
-        "project_domain_name": get_or_else("project_domain_name"),
-        "project_id": get_or_else("project_id"),
-        "system_scope": get_or_else("system_scope"),
-        "trust_id": get_or_else("trust_id"),
-        "user_domain_id": get_or_else("user_domain_id"),
-        "user_domain_name": get_or_else("user_domain_name"),
-        "user_id": get_or_else("user_id"),
-        "username": get_or_else("username")
-    }
-
-def make_auth(instance_auth_url, conf, log):
-    """Build a new Authentication plugin (Password based).
+def make_admin_auth(instance_auth_url, log):
+    """Build a new Authentication plugin for admin (Password based).
 
     Args:
         instance_auth_url (str): Identity service endpoint for authentication,
             e.g., "http://10.0.2.15:80/identity". Do not add the '/v3'!
-        conf (dict): A dictionary with keys of
-            `keystoneauth1.identity.v3.Passwords` [1]. Note that `*_id` and
-            `*_name` keys can be filled in an exclusive manner (i.e., only fill
-            `*_id` or `*_name`)
         log (logging.Logger): Logger for debug information.
 
     Returns:
@@ -99,21 +72,25 @@ def make_auth(instance_auth_url, conf, log):
 
     Refs:
         [1] https://docs.openstack.org/keystoneauth/rocky/api/keystoneauth1.identity.v3.html#keystoneauth1.identity.v3.Password
-
+        [2] https://developer.openstack.org/api-ref/identity/v3/?expanded=password-authentication-with-unscoped-authorization-detail,password-authentication-with-scoped-authorization-detail#password-authentication-with-scoped-authorization
     """
-    log.debug("New authentication for %s with %s"
-              % (instance_auth_url, conf))
+    log.debug("New authentication for %s with admin" % instance_auth_url)
     auth = v3.Password(
+        # Use Admin credential -- Same everywhere in this PoC!
+        project_domain_id='default',
+        user_domain_id='default',
+        username='admin',
+        password='admin',
         # The `plugin_creator` of `_create_auth_plugin` automatically add the
         # V3, but here we have to manually add it.
         auth_url="%s/v3" % instance_auth_url,
-        # Allow fetching a new token if the current one is goind to expire
+        # Allow fetching a new token if the current one is going to expire
         reauthenticate=True,
-        # Mandatory to get the service catalog fill properly
-        project_name='service',  # for project's scoping
+        # Project scoping is mandatory to get the service catalog fill properly
+        # See [2].
+        project_name='admin',    # for project's scoping
         include_catalog=True,    # include the service catalog in the token
-        # Get the rest from the conf
-        **conf)
+    )
 
     log.debug("Authentication plugin %s" % vars(auth))
     return auth
@@ -128,7 +105,7 @@ def make_keystone_client(instance_name, session, log):
         interface='admin',
         region_name=instance_name)
 
-    # XXX: Is it really needed
+    # XXX: Is it really needed?
     # auth_version = conf.get('auth_version')
     # if auth_version is not None:
     #     auth_version = discover.normalize_version_number(auth_version)
@@ -136,17 +113,17 @@ def make_keystone_client(instance_name, session, log):
     k_client = _identity.IdentityServer(log, adapter)
     log.debug("Success keystone client on %s" % k_client.www_authenticate_uri)
 
-    # XXX: Is it really needed
+    # XXX: Is it really needed?
     # include_service_catalog=conf.get('include_service_catalog'),
     # requested_auth_version=auth_version)
     return k_client
 
 
-def get_keystone_client(instance_auth_url, instance_name, conf, log):
+def get_admin_keystone_client(instance_auth_url, instance_name, log):
     """Get or Lazily create a keystone client on `instance_auth_url`.
 
     Lookup into `K_CLIENTS` for a keystone client on `instance_auth_url`.
-    Creates it if misses and returns it.
+    Creates an admin client if misses and returns it.
 
     Args:
         instance_auth_url (str): Identity service endpoint for authentication,
@@ -155,11 +132,6 @@ def get_keystone_client(instance_auth_url, instance_name, conf, log):
         instance_name (str): Name of the Instance as in services.json (e.g,
             InstanceOne, InstanceTwo, ...).
 
-        conf (dict): A dictionary with keys of
-            `keystoneauth1.identity.v3.Passwords` [1]. Note that `*_id` and
-            `*_name` keys can be filled in an exclusive manner (i.e., only fill
-            `*_id` or `*_name`)
-
         log (logging.Logger): Logger for debug information.
 
     Returns:
@@ -167,7 +139,7 @@ def get_keystone_client(instance_auth_url, instance_name, conf, log):
 
     """
     if instance_auth_url not in K_CLIENTS:
-        auth = make_auth(instance_auth_url, conf, log)
+        auth = make_admin_auth(instance_auth_url, log)
         sess = Session(auth=auth)
         k_client = make_keystone_client(instance_name, sess, log)
 
@@ -190,7 +162,6 @@ def target_good_keystone(f):
         cls (BaseAuthProtocol): Reference to a BaseAuthProtocol middleware.
 
         """
-
         # Make a copy of the middleware instance every-time someone process a
         # request for thread safety (since we change its state).
         kls = copy.copy(cls)
@@ -205,16 +176,11 @@ def target_good_keystone(f):
         # Get the proper Keystone client and unpdate `kls` middleware in
         # regards.
         #
-        # In this PoC, we know that `[keystone_authtoken]` of nova in
-        # InstanceOne shares the same configuration -- admin, password,
-        # project_name, ... -- as nova in InstanceTwo, InstanceThree, ...
-        # because we run the same Devstack multiple times. Hence, we can rely
-        # on the `kls._conf` object of the current instance (i.e.,
-        # `original_auth_url`) even if we try to connect to keystone of another
-        # instance (i.e., `instance_auth_url`). And this is true for all
-        # services, shuch as glance, neutron, ...
-        (auth, sess, k_client) = get_keystone_client(
-            instance_auth_url, instance_name, lift_conf(kls._conf), kls.log)
+        # In this PoC, we know that every OpenStack instance is Devstack based.
+        # Hence, we can rely on admin user to connect to Keystone of another
+        # instance (i.e., `instance_auth_url`)..
+        (auth, sess, k_client) = get_admin_keystone_client(
+            instance_auth_url, instance_name, kls.log)
         kls._auth = auth
         kls._session = sess
         kls._identity_server = k_client
